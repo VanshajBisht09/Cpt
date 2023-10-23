3459884cb127ed798a6a0488fc8dab028991a006
VanshajBisht09 <125120847+VanshajBisht09@users.noreply.github.com>
2023-10-24 00:18:48 +0530

    Update About.java

--- src/GUI/About.java
+++ src/GUI/About.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package GUI;
 
-/**
- *
- * @author Dulan
- */
 public class About extends javax.swing.JInternalFrame {
 
     /**
